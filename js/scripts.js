9902aa9dddf93ace50cad8b2a0e1d76783c40de1
Micah Zaayer <micahzaayer@gmail.com>
2019-08-23 10:42:13 -0700

    add different loops. Trying to avoid infinite loops...

--- js/scripts.js
+++ js/scripts.js
@@ -3,53 +3,51 @@ $(document).ready(function(){
   $("#form").submit(function(event){
     event.preventDefault();
   var userInput = $("#userInput").val();
-  var numString = userInput.split("");
+  // var numString = userInput.split("");
   var beep = ["Beep!"]
   var boop = ["Boop!"]
   var sorry = ["I'm sorry, David. I'm afraid I cannot do that."]
   var nums = ["1", "2", "3"];
   var newArray = [];
 
-console.log(numString);
-
-
-  // for (var i = 0; i <= userInput; i++) {
-  //   $("#list").append("<li>" + i + "</li>");
-  // };
-
-//   for (var i = 0; i < userInput; i++) {
-//     if ("1"===numString[i]) {
-//       newArray.push("beep!");
-//     }
-//     else if ("2"===(numString[i])) {
-//       newArray.push("boop");
-//     }
-//     else if ("3"===(numString[i])) {
-//       newArray.push("sorry");
-//     }
-//     else {
-//       newArray.push(numString[i]);
-//       console.log(newArray);
-//     }
-//     $("#list").append(newArray[i]);
-// }
 
   for (var i = 0; i <= userInput; i++) {
-    if ("1".includes(numString[i])) {
+    newArray.push(i);
+    $("#list").append("<li>" + i + "</li>");
+  };
+
+
+  for (var i = 0; i < newArray.length; i++) {
+    if ("1".includes(newArray[i])) {
       newArray.push("beep!");
     }
-    else if ("2".includes(numString[i])) {
+    else if ("2".includes(newArray[i])) {
       newArray.push("boop");
     }
-    else if ("3".includes(numString[i])) {
-      newArray.slice("sorry");
+    else if ("3".includes(newArray[i])) {
+      newArray.push("sorry");
     }
     else {
-      newArray.push([i]);
+      newArray.push(newArray[i]);
       console.log(newArray);
     }
-    $("#list").append(newArray[i] + ", ");
 }
+
+
+    // if ("1".includes(userInput[i])) {
+    //   newArray.push("beep!");
+    // }
+    // else if ("2".includes(userInput[i])) {
+    //   newArray.push("boop");
+    // }
+    // else if ("3".includes(userInput[i])) {
+    //   newArray.push("sorry");
+    // }
+    // else {
+    //   newArray.push([i]);
+    //   console.log(newArray);
+    // }
+
 
 
 
